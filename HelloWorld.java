2831950467ad4282bbee1dd32c9414f268dd0074
Gantogtokh Battumur <b.ganaa592@gmail.com>
2020-11-23 20:53:36 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,32 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World! - Andre Goncalves, Brazil" );
+          System.out.println("Hello World! - Gantogtokh Battumur, Mongolia" );
     }
-     //Enter your command line bellow, so we can test out the shared file and edition, please! :)
+      public class Method {
+ static void GantogtokhMethod(){
+    System.out.println("My name is Gantogtokh Battumur, this is my code");
+ }
+    public static void main(String[] args) {
+      System.out.println("This is main program");
+      GantogtokhMethod();
+              System.out.println("This is main program");
+      GantogtokhMethod();
+      System.out.println("Please enter a number");
+
+
+Scanner myKb = new Scanner(System.in);
+int num3 = myKb.nextInt();
+
+System.out.println("Please enter another number");
+int num4 = myKb.nextInt();
+
+System.out.println("The divide  of " + num3 + " and " + num4 + " is " + (num3 / num4));
+
+
+        
+    }
+        
 }
+
+
