4ac231251b9af25738abaf4389244902286c1bf7
Gantogtokh Battumur <b.ganaa592@gmail.com>
2020-11-23 22:17:41 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -42,7 +42,7 @@ public static void main(String[] args) {
 
 System.out.println("Hello World! - Andre Goncalves, Brazil ");
 System.out.println("Hello World! Lucas Gustavo, Brazil ");
-    
+System.out.println("Hello World! Gantogtokh Battumur, Mongolia ");    
 calculator_sum();    
 caclulator_multiplication();
 
